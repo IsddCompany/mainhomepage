5d4d0fdf9a4b6042d52d46aa8d21bcc7bd60d2e4
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2024-05-09 10:41:18 +0900

    2024-05-09-1040
    집가서 얼른 만들어라 쭌!
    일해라 쭌!

--- login/js/login.js
+++ login/js/login.js
@@ -1,12 +1,20 @@
-
-var password = document.getElementById("password")[0];
-var email = document.getElementById("email")[0];
+var password = document.getElementsByClassName("password")[0];
+var email = document.getElementsByClassName("email")[0];
 
 function login() {
-    if (password.value.length == 0) {
+    if (email.value == '' && password.value == 0){
+        alert("이메일 또는 아이디, 비밀번호를 입력해주십시오")
+    }
+    else if(password.value == 0) {
         alert("비밀번호를 입력해주십시오")
     }
-    else if (email.value.length == 0) {
-        alert("이메일 또는 아이디를 입력해주십시오")
+    else if (email.value == '') {
+        alert("이메일을 입력해 주십시오")
+    }
+    else if(email.value == "0" && password.value == "0") {
+        //이건 나중에 집가서 ㄱㄱ        
+    }
+    else{
+        alert("비밀번호 또는 아이디가 옳지 않습니다.")
     }
 }
